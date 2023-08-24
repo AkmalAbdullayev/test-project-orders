d1603a4aed632c5dec5fd33f039564ef5eb028d9
Akmal Abdullayev <muhiddinovich@Akmals-MacBook-Pro.local>
2023-08-24 15:05:36 +0500

    creating index route and ApiResource for index page

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\RequestResource;
+use App\Models\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class RequestController extends Controller
 {
-    //
+    public function index(): AnonymousResourceCollection
+    {
+        $requests = Request::query()->paginate(10);
+
+        return RequestResource::collection($requests);
+    }
 }
